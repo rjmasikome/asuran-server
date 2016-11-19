eefe71a0a240050c15104149e92801e5e2d06597
RJ Masikome <rjmasikome@gmail.com>
2016-11-19 18:08:02 +0100

    Website registration

--- website/js/contact_me.js
+++ website/js/contact_me.js
@@ -9,26 +9,30 @@ $(function() {
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
-            
+
             // get values from FORM
-            var name = $("input#name").val();
+            var firstName = $("input#first-name").val();
+            var lastName = $("input#last-name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
+            var password = $("textarea#password").val();
+
+            var sendObj = {
+              name: {
+                first: firstName,
+                last: lastName
+              },
+              email: email,
+              login: {
+                username: email,
+                password: $.md5(password)
+              }
+            };
+
+
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://localhost:8080/setUser",
                 type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
+                data: sendObj,
                 cache: false,
                 success: function() {
                     // Enable button & show success message
@@ -37,7 +41,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>You have been registered, please login in our app using your email.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -49,7 +53,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that the server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
